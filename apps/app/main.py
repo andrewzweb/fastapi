cf4ee4f05ed050c7e119ca12a1ed258ce83efa6f
andrewzweb <andrewzweb@gmail.com>
2022-08-07 16:04:44 +0300

    small refactoring

--- apps/app/main.py
+++ apps/app/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
-
-#from app.ping.api import ping_pong
 import app.ping.api as ping
-#app = FastAPI()
 
-#app.include_router(ping_pong.router)
 
 def create_app():
     app = FastAPI()
